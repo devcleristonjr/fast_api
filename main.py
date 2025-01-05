6c2cfb514769539d2c19267848d209233ae099a2
devcleristonjr <peros31@hotmail.com>
2025-01-05 14:13:56 -0300

    eighth commit

--- main.py
+++ main.py
@@ -5,7 +5,7 @@ from database import engine, sessionlocal
 from sqlalchemy.orm import Session
 from fastapi.responses import RedirectResponse
 from fastapi.staticfiles import StaticFiles
-from pydantic import BaseModel
+from pydantic import BaseModel, Field
 
 models.Base.metadata.create_all(bind=engine)
 templates = Jinja2Templates(directory="templates")
@@ -15,11 +15,11 @@ app.mount("/static", StaticFiles(directory="static"), name="static")
 
 class TaskItem(BaseModel):
     id: int | None = None
-    name: str
-    description: str
+    name: str = Field(..., example="Buy groceries")
+    description: str | None = Field(None, example="Buy milk, eggs, and bread")
     created_at: str | None = None
     updated_at: str | None = None
-    progress: str
+    progress: str = Field(..., example="open")
 
 def get_db():
     db = sessionlocal()
@@ -31,7 +31,7 @@ def get_db():
 # Endpoints para backend (Insomnia)
 
 # Endpoint para criar uma nova tarefa via POST
-@app.post("/tasks/create")
+@app.post("/tasks/create", response_description="Create a new task")
 async def create_task(taskItem: TaskItem, db: Session = Depends(get_db)):
     task = models.Task(
         name=taskItem.name,
@@ -44,13 +44,13 @@ async def create_task(taskItem: TaskItem, db: Session = Depends(get_db)):
     return {"message": "Task created successfully", "task": task}
 
 # Endpoint para listar todas as tarefas
-@app.get("/tasks")
+@app.get("/tasks", response_description="List all tasks")
 async def list_tasks(db: Session = Depends(get_db)):
     tasks = db.query(models.Task).order_by(models.Task.id.desc()).all()
     return {"tasks": tasks}
 
 # Endpoint para atualizar uma tarefa existente
-@app.put("/tasks/update/{task_id}")
+@app.put("/tasks/update/{task_id}", response_description="Update an existing task")
 async def update_task(task_id: int, taskItem: TaskItem, db: Session = Depends(get_db)):
     task = db.query(models.Task).filter(models.Task.id == task_id).first()
     if not task:
@@ -65,7 +65,7 @@ async def update_task(task_id: int, taskItem: TaskItem, db: Session = Depends(ge
     return {"message": "Task updated successfully", "task": task}
 
 # Endpoint para deletar uma tarefa
-@app.delete("/tasks/delete/{task_id}")
+@app.delete("/tasks/delete/{task_id}", response_description="Delete a task")
 async def delete_task(task_id: int, db: Session = Depends(get_db)):
     task = db.query(models.Task).filter(models.Task.id == task_id).first()
     if not task:
@@ -76,7 +76,7 @@ async def delete_task(task_id: int, db: Session = Depends(get_db)):
     return {"message": "Task deleted successfully"}
 
 # Endpoint para visualizar uma tarefa específica pelo ID
-@app.get("/tasks/{task_id}")
+@app.get("/tasks/{task_id}", response_description="Get a specific task by ID")
 async def get_task(task_id: int, db: Session = Depends(get_db)):
     task = db.query(models.Task).filter(models.Task.id == task_id).first()
     if not task:
@@ -87,13 +87,13 @@ async def get_task(task_id: int, db: Session = Depends(get_db)):
 # Endpoints para front-end
 
 # Rota principal que exibe a lista de tasks
-@app.get("/")
+@app.get("/", response_description="Home page displaying the list of tasks")
 async def home(request: Request, db: Session = Depends(get_db)):
     tasks = db.query(models.Task).order_by(models.Task.id.desc()).all()
     return templates.TemplateResponse("index.html", {"request": request, "tasks": tasks})
 
 # Rota para adicionar um novo tasks
-@app.post("/add")
+@app.post("/add", response_description="Add a new task")
 async def add(
         request: Request, 
         name: str = Form(...),
@@ -113,12 +113,12 @@ async def add(
     return RedirectResponse(url=app.url_path_for("home"), status_code=status.HTTP_303_SEE_OTHER)
 
 # Rota para exibir o formulário de adição de novo tasks
-@app.get("/addnew")
+@app.get("/addnew", response_description="Form to add a new task")
 async def addnew(request: Request):
     return templates.TemplateResponse("addnew.html", {"request": request})
 
 # Rota para exibir os detalhes de um tasks
-@app.get("/task/{task_id}")
+@app.get("/task/{task_id}", response_description="View details of a specific task")
 async def get_task(task_id: int, request: Request, db: Session = Depends(get_db)):
     task = db.query(models.Task).filter(models.Task.id == task_id).first()
     if not task:
@@ -127,13 +127,13 @@ async def get_task(task_id: int, request: Request, db: Session = Depends(get_db)
     return templates.TemplateResponse("task_detail.html", {"request": request, "task": task})
 
 # Rota para exibir o formulário de edição de um tasks
-@app.get("/edit/{task_id}")
+@app.get("/edit/{task_id}", response_description="Form to edit a specific task")
 async def edit(request: Request, task_id: int, db: Session = Depends(get_db)):
     task = db.query(models.Task).filter(models.Task.id == task_id).first()
     return templates.TemplateResponse("edit.html", {"request": request, "task": task})
 
 # Rota para atualizar os dados de um tasks
-@app.post("/update/{task_id}")
+@app.post("/update/{task_id}", response_description="Update a specific task")
 async def update(request: Request, task_id: int, name: str = Form(...), description: str = Form(...), progress: str = Form(...), method: str = Form(None), db: Session = Depends(get_db)):
     if method == 'put':  # Verifica se estamos simulando o PUT
         task = db.query(models.Task).filter(models.Task.id == task_id).first()
@@ -147,8 +147,9 @@ async def update(request: Request, task_id: int, name: str = Form(...), descript
         return RedirectResponse(url=app.url_path_for("home"), status_code=status.HTTP_303_SEE_OTHER)
 
     return {"error": "Invalid method"}
+
 # Rota para excluir um tasks
-@app.post("/remove/{task_id}")
+@app.post("/remove/{task_id}", response_description="Remove a specific task")
 async def remove(request: Request, task_id: int, db: Session = Depends(get_db)):
     form_data = await request.form()
     method = form_data.get("_method")
@@ -159,4 +160,3 @@ async def remove(request: Request, task_id: int, db: Session = Depends(get_db)):
             db.commit()
         return RedirectResponse(url=app.url_path_for("home"), status_code=status.HTTP_303_SEE_OTHER)
     return {"error": "Invalid method"}
-
